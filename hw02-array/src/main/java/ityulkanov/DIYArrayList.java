f97dd39c8d8a9ec0c8b573a9785cf914413f2927
Igor Tyulkanov <ityulkanov@ibs.ru>
2020-04-23 15:37:45 +0300

    listiterator

--- hw02-array/src/main/java/ityulkanov/DIYArrayList.java
+++ hw02-array/src/main/java/ityulkanov/DIYArrayList.java
@@ -146,8 +146,56 @@ public class DIYArrayList<T> implements List<T> {
 
     @Override
     public ListIterator<T> listIterator() {
-        throw new UnsupportedOperationException();
 
+        int counter;
+        int lastRet;
+
+        return new ListIterator<T>() {
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public T next() {
+                return null;
+            }
+
+            @Override
+            public boolean hasPrevious() {
+                return false;
+            }
+
+            @Override
+            public T previous() {
+                return null;
+            }
+
+            @Override
+            public int nextIndex() {
+                return 0;
+            }
+
+            @Override
+            public int previousIndex() {
+                return 0;
+            }
+
+            @Override
+            public void remove() {
+
+            }
+
+            @Override
+            public void set(T t) {
+
+            }
+
+            @Override
+            public void add(T t) {
+
+            }
+        };
     }
 
     @Override
