7aeedcb3a0a55de3005f856f27a106f8a602e1bb
Igor Tyulkanov <ityulkanov@ibs.ru>
2020-04-24 09:32:49 +0300

    Preffity output

--- hw03-reflections/src/main/java/com/ityulkanov/StarterClass.java
+++ hw03-reflections/src/main/java/com/ityulkanov/StarterClass.java
@@ -34,7 +34,7 @@ public class StarterClass {
         System.out.printf("We've runnned total of %d tests\n", counter);
         System.out.println("################################################");
 
-        System.out.printf("Total tests successful: %d\n", counter - exceptionCounter);
+        System.out.printf("Total tests were successful: %d\n", counter - exceptionCounter);
         System.out.println("################################################");
 
         System.out.printf("Total tests failed:  %d\n", exceptionCounter);
